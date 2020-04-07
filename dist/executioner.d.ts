c2ecfc4798ac369b31c8825f50bdf91e327bc0ab
thibault <thibault.gerrier@student.uibk.ac.at>
2020-04-07 15:09:19 +0200

    export

--- dist/executioner.d.ts
+++ dist/executioner.d.ts
@@ -3,6 +3,6 @@ import { evalPath } from "./paths";
 import { takeAll } from "./utils";
 import { Graph } from "./graph";
 import { NamedNode, Literal } from "./term";
-declare type OutFn = (base: string, spp: string, prefix?: Record<string, string>) => string[];
-export declare const SPPEvaluator: (doc: any, inputType: InputType) => Promise<[OutFn, Graph]>;
+export declare type SPPEval = (base: string, spp: string, prefix?: Record<string, string>) => string[];
+export declare const SPPEvaluator: (doc: any, inputType: InputType) => Promise<[SPPEval, Graph]>;
 export { evalPath, Literal, NamedNode, takeAll };
